35377347df64c4979fb98a30519b19315e118c5e
Artemis-unity <muijs@student.chalmers.se>
2025-11-04 13:58:29 +0000

    Why is it not working

--- Assets/Samples/Universal Render Pipeline/17.1.0/URP RenderGraph Samples/BlitWithMaterial/BlitAndSwapColorRendererFeature.cs
+++ Assets/Samples/Universal Render Pipeline/17.1.0/URP RenderGraph Samples/BlitWithMaterial/BlitAndSwapColorRendererFeature.cs
@@ -29,12 +29,20 @@ public class BlitAndSwapColorPass : ScriptableRenderPass
         //It's good practice to set it here and not from the RenderFeature. This way, the pass is selfcontaining and you can use it to directly enqueue the pass from a monobehaviour without a RenderFeature.
         requiresIntermediateTexture = true;
     }
-
-    public override void RecordRenderGraph(RenderGraph renderGraph, ContextContainer frameData)
-    {
+    class PassData {
+        public TextureHandle backgroundTexture;
+        public TextureHandle tempBlurTexture;
+    }
+    public override void RecordRenderGraph(RenderGraph renderGraph, ContextContainer frameData) {
+        using var builder = renderGraph.AddRasterRenderPass<PassData>("Create yellow texture", out var passData);
         // UniversalResourceData contains all the texture handles used by the renderer, including the active color and depth textures
         // The active color and depth textures are the main color and depth buffers that the camera renders into
         var resourceData = frameData.Get<UniversalResourceData>();
+
+        // Create a texture the size of the screen
+        TextureDesc textureDesc = resourceData.activeColorTexture.GetDescriptor(renderGraph);
+        TextureHandle textureHandle = renderGraph.CreateTexture(textureDesc);
+        builder.UseTexture(textureHandle); // Read texture? 
 
         //This should never happen since we set m_Pass.requiresIntermediateTexture = true;
         //Unless you set the render event to AfterRendering, where we only have the BackBuffer. 
@@ -47,7 +55,6 @@ public class BlitAndSwapColorPass : ScriptableRenderPass
         // The destination texture is created here, 
         // the texture is created with the same dimensions as the active color texture
         var source = resourceData.activeColorTexture;
-
         var destinationDesc = renderGraph.GetTextureDesc(source);
         destinationDesc.name = $"CameraColor-{m_PassName}";
         destinationDesc.clearBuffer = false;
--- Assets/Shaders/BackgroundBlurRenderFeature.cs
+++ Assets/Shaders/BackgroundBlurRenderFeature.cs
@@ -1,23 +1,26 @@
 using System;
+using System.Security.Cryptography;
 using UnityEngine;
 using UnityEngine.Rendering;
 using UnityEngine.Rendering.RendererUtils;
 using UnityEngine.Rendering.RenderGraphModule;
 using UnityEngine.Rendering.RenderGraphModule.Util;
 using UnityEngine.Rendering.Universal;
+using static UnityEngine.Rendering.Universal.ShaderInput;
 // Following https://docs.unity3d.com/6000.1/Documentation/Manual/urp/renderer-features/create-custom-renderer-feature.html
 public class BackgroundBlurRenderFeature : ScriptableRendererFeature {
     [SerializeField] private BlurSettings settings;
     [SerializeField] private Shader shader;
     private Material material;
     private BlurRenderPass blurRenderPass;
+    public LayerMask layerMask;
     // Like start but for shaders
     public override void Create() {
         if (shader == null) {
             return;
         }
         material = new Material(shader);
-        blurRenderPass = new BlurRenderPass(material, settings);
+        blurRenderPass = new BlurRenderPass(material, settings, layerMask);
 
         blurRenderPass.renderPassEvent = RenderPassEvent.BeforeRenderingPostProcessing;
     }
@@ -28,7 +31,7 @@ public class BackgroundBlurRenderFeature : ScriptableRendererFeature {
             return;
         }
         if (renderingData.cameraData.cameraType == CameraType.Game) {
-            Debug.Log("QUEIQUERHIUH");
+            //Debug.Log("QUEIQUERHIUH");
             renderer.EnqueuePass(blurRenderPass);
         }
     }
@@ -51,25 +54,28 @@ public class BackgroundBlurRenderFeature : ScriptableRendererFeature {
 
         private BlurSettings defaultSettings;
         private Material material;
+        private LayerMask m_LayerMask;
 
         private TextureDesc blurTextureDescriptor;
 
         // Constructor!
-        public BlurRenderPass(Material material, BlurSettings defaultSettings) {
+        public BlurRenderPass(Material material, BlurSettings defaultSettings, LayerMask mask) {
             this.material = material;
             this.defaultSettings = defaultSettings;
+            m_LayerMask = mask;
         }
 
         // Called every frame once for each camera, adds and configures render passes in the render graph. 
         public override void RecordRenderGraph(RenderGraph renderGraph, ContextContainer frameData) {
-            /*
+            
               
             // Using a profiling scope ensures that our pass shows up in the Frame Debugger
             using var builder = renderGraph.AddRasterRenderPass<PassData>("Blur Background Pass", out var passData);
             // 1. GATHERING RESOURCES & DATA
             UniversalResourceData resourceData = frameData.Get<UniversalResourceData>();
             UniversalCameraData cameraData = frameData.Get<UniversalCameraData>();
-
+            UniversalRenderingData universalRenderingData = frameData.Get<UniversalRenderingData>();
+            UniversalLightData lightData = frameData.Get<UniversalLightData>();
             // Get the camera's color target. This is what we will eventually draw TO.
             TextureHandle cameraColorTarget = resourceData.activeColorTexture;
 
@@ -84,26 +90,28 @@ public class BackgroundBlurRenderFeature : ScriptableRendererFeature {
             // 3. DEFINING THE WORKFLOW
 
             // --- PASS 1: Render the specified layers into our temporary texture ---
-            var blitParams = new RenderGraphUtils.BlitMaterialParameters(passData.backgroundTexture,)
-            // Define the renderers we want to draw (based on layer mask).
-            // This is the key step for isolating the objects!
-            var rendererListDesc = new RendererListDesc(new ShaderTagId("UniversalForward"), cameraData.cullingResults, cameraData.camera) {
-                layerMask = _settings.layersToBlur,
-                renderQueueRange = RenderQueueRange.all,
-                sortingCriteria = SortingCriteria.CommonOpaque,
-            };
-            passData.rendererList = builder.UseRendererList(renderGraph.CreateRendererList(rendererListDesc));
-            builder.UseRendererList()
+            var sortFlags = cameraData.defaultOpaqueSortFlags;
+            RenderQueueRange renderQueueRange = RenderQueueRange.opaque;
+            FilteringSettings filterSettings = new FilteringSettings(renderQueueRange, m_LayerMask);
+
+            DrawingSettings drawSettings = RenderingUtils.CreateDrawingSettings(new ShaderTagId("SRPDefaultUnlit"), universalRenderingData, cameraData, lightData, sortFlags);
+            var param = new RendererListParams(universalRenderingData.cullResults, drawSettings, filterSettings);
+
+            passData.rendererList = renderGraph.CreateRendererList(param);
+            //passData.rendererList = builder.UseRendererList(renderGraph.CreateRendererList(rendererListDesc));
+            
             // Set our background texture as the render target for this draw call.
             // We must clear it to transparent black so we don't get garbage from previous frames.
-            builder.SetRenderAttachment(passData.backgroundTexture, 0, AccessFlags.Write, ClearFlag.Color, Color.clear);
-
+            builder.SetRenderAttachment(passData.backgroundTexture, 0);
             // Execute the draw call. This draws the sprites from the 'BlurrableBackground' layer.
-            builder.DrawRendererList(passData.rendererList);
+            //builder.DrawRendererList(passData.rendererList);
 
+            builder.SetRenderFunc((PassData data, RasterGraphContext context) => ExecutePass(data, context));
+
+            builder.SetRenderFunc<PassData>(ExecutePass);
 
             // --- PASS 2 & 3: Perform the two-pass blur ---
-
+            /*
             _settings.blurMaterial.SetFloat(BlurSizeID, _settings.blurStrength);
 
             // Vertical Blur: from our background texture -> to a temporary texture
@@ -124,17 +132,11 @@ public class BackgroundBlurRenderFeature : ScriptableRendererFeature {
             // Set the pass's execution function
             builder.SetRenderFunc((PassData data, RasterGraphContext context) => ExecutePass(data, context));
 
-            // The AddBlitPass method adds a vertical blur render graph pass that blits from the source texture (camera color in this case) to the destination texture using the first shader pass (the shader pass is defined in the last parameter).
-            //RenderGraphUtils.BlitMaterialParameters paraVertical = new(srcCamColor, dst, material, 0);
-            //renderGraph.AddBlitPass(paraVertical, k_VerticalPassName);
-            //
-            //// The AddBlitPass method adds a horizontal blur render graph pass that blits from the texture written by the vertical blur pass to the camera color texture. The method uses the second shader pass.
-            //RenderGraphUtils.BlitMaterialParameters paraHorizontal = new(dst, srcCamColor, material, 1);
-            //renderGraph.AddBlitPass(paraHorizontal, k_HorizontalPassName);
+   
             */
         }
         private static void ExecutePass(PassData data, RasterGraphContext context) {
-            
+            context.cmd.DrawRendererList(data.rendererList);
         }
         // The data that our pass needs to execute.
         private class PassData {
