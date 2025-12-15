77882ed5987a7b310e0c78c0b16e3597cdd4862d
r8teful <r8tefulgames@gmail.com>
2025-12-15 20:50:11 +0100

    Ability icon
    Yeah it needs some work. Next up I'm doing upgrades and it will break it so this commit is also like a safety lol

--- Assets/Scripts/UI/UIHudIconAbilityActive.cs
+++ Assets/Scripts/UI/UIHudIconAbilityActive.cs
@@ -2,10 +2,11 @@
 using System.Collections;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 // Shows active abilities with cooldown and active length
 public class UIHudIconAbilityActive : UIHudIconBase {
-
+    [SerializeField] private Image _progressImage;
     [SerializeField] private TextMeshProUGUI timeText; // We won't really have a time text but just for now
     internal void Init(AbilityInstance ability) {
         InitBase(ability.Data.icon, ability.Data.displayName, "");
@@ -18,10 +19,10 @@ public class UIHudIconAbilityActive : UIHudIconBase {
 
 
     private void OnAbilityReady() {
-        _iconImage.color = Color.green;
+        //_iconImage.color = Color.green;
     }
     private void OnAbilityUsed() {
-        _iconImage.color = Color.white;
+        //_iconImage.color = Color.white;
     }
 
     private void OnAbilityActiveTimeChanged(float time) {
@@ -31,15 +32,9 @@ public class UIHudIconAbilityActive : UIHudIconBase {
         
     }
     private void OnAbilityCooldownChanged(float time) {
-        // THINK ABOUT IT!!
-        // ability cooldown is just the cooldown until we can activate the ability next, we should show this only when the ability isn't currently active
-        // If it is active, we have to show how long it will be active for
-
-        // the wierd thing is that the buff itself is not on our AbilityInstance, it is on the lazer abilityinstance. So we have two options:
-        // We either get a reference to the BuffInstance that IS on the lazer instance, or, we just say "brimstone has been fired with this buffInstance".
-        // 
-        timeText.text = time.ToString();
-        timeText.color = Color.white;
+        _progressImage.fillAmount = time;
+        //timeText.text = time.ToString();
+        //timeText.color = Color.white;
     }
     string FormatSeconds(float sec) {
         if (sec <= 0) return "0s";
